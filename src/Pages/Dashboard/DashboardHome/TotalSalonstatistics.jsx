06c6615d75420e40bbb3a07f8a9a31ae1fd47b4e
shaharul siyam <shaharulsiyam0273@gmail.com>
2024-07-14 15:59:48 +0600

    profile

--- src/Pages/Dashboard/DashboardHome/TotalSalonstatistics.jsx
+++ src/Pages/Dashboard/DashboardHome/TotalSalonstatistics.jsx
@@ -1,6 +1,9 @@
+import { Dropdown } from 'antd';
+import { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import { DownOutlined } from "@ant-design/icons";
 const TotalSalonstatistics = () => {
+    const [year, setYear] = useState(2024)
     const data = [
         {
             name: 'jan',
@@ -75,8 +78,46 @@ const TotalSalonstatistics = () => {
             amt: 2100,
         },
     ];
+    const items = [
+        {
+            label: 2023,
+            key: "2023",
+        },
+        {
+            label: 2024,
+            key: "2024",
+        },
+        {
+            label: 2025,
+            key: "2025",
+        },
+        {
+            label: 2026,
+            key: "2026",
+        },
+    ];
+    const onClick = ({ key }) => {
+        setYear(key)
+    };
     return (
         <div className='w-full h-[400px]'>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                <p style={{ marginTop: "10px", fontSize: "20px", fontWeight: 600, marginBottom: "10px", color: "black", }}>Total Salon statistics</p>
+                <Dropdown menu={{ items, onClick }} >
+                    <p style={{
+                        // width: "79px", 
+                        cursor: "pointer",
+                        color: '#717171',
+                        border: "1px solid #E9E9E9",
+                        borderRadius: "4px",
+                        padding: "4px 12px"
+                    }} onClick={(e) => e.preventDefault()}
+                    >
+                        {year}
+                        <DownOutlined style={{ paddingLeft: "18px" }} color='#717171' />
+                    </p>
+                </Dropdown>
+            </div>
             <ResponsiveContainer width="100%" height="100%">
                 <LineChart
                     width={500}
